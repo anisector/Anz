58f6a5c3c182c1ea054f86df43c267a8131fa87a
anisector <144573755+anisector@users.noreply.github.com>
2026-08-28 01:42:17 +0300

    Copy AniziumPlugin.kt to Anz module

---
+++ Anz/src/main/kotlin/com/kerimmkirac/AniziumPlugin.kt
@@ -0,0 +1,14 @@
+package com.kerimmkirac
+
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+
+@CloudstreamPlugin
+class AniziumPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(Anizium())
+    }
+}
+
