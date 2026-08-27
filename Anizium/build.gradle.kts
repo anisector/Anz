2c70de528ac629c7c469b72b5761fad376336967
anisector <144573755+anisector@users.noreply.github.com>
2026-08-28 01:42:56 +0300

    Remove old Anizium module file

--- Anizium/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 28
-cloudstream {
-    description = "Anizium provider - current API + CDN fallback"
-    authors = listOf("kerimmkirac", "ByAyzen", "anisector")
-    status = 1
-    tvTypes = listOf("Anime", "AnimeMovie")
-    language = "tr"
-    iconUrl = "https://anizium.co/assets/images/logo.png"
-}
